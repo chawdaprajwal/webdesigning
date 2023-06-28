c50f496c268495c2297849f391308e8f24f93831
chawdalprajwal <lopezloyla@gmail.com>
2023-06-28 10:18:55 +0530

    save file

--- MOD 2/JS/datatypes.js
+++ MOD 2/JS/datatypes.js
@@ -40,7 +40,38 @@
 // }
 
 
-for (i = 2; i<=10;i+=2)
-{
-    console.log(i);
+// for (i = 2; i<=10;i+=2)
+// {
+//     console.log(i);
+// }
+// i = 1
+// while(i<=10)
+// {
+//     console.log(i);
+//     i = i + 1;
+// }
+
+
+//sum of ten numbers
+
+// const number = 10;
+// let sum =0;
+// for (i = 0; i <= number ;i++)
+// {
+//     sum = sum + i;
+   
+// }
+// console.log('sum of 10 numbers is :',sum);
+
+
+
+//while
+
+var p = 1;
+i=1;
+while(i<=10){
+    p = p*i;
+    i = i + 1;
+
 }
+console.log('Product of 10 numbers',p);
