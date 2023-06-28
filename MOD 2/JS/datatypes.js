99098384ed353050357f5b05e3ab24fe801d9592
chawdalprajwal <lopezloyla@gmail.com>
2023-06-28 09:59:10 +0530

    save file html

---
+++ MOD 2/JS/datatypes.js
@@ -0,0 +1,46 @@
+/*variable*/ 
+// var a  = 5;
+// var b  = 6.5;
+// var c = "hi@1";
+// var d = true;
+// var  e;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+
+/*function*/
+// let b =5;
+// function f(){
+//     var a = 40;
+//     var b= 6;
+
+// }
+// console.log(b);
+
+/*nested if .js*/
+// var a = 95;
+// if(a>75)
+// {
+//     console.log("Destinction");
+// }
+// else if(a>60){
+//     console.log("First class");
+// }
+// else if(a>40){
+//     console.log("pass");
+// }
+
+
+//if else
+
+// for (i = 1; i<=10;i++)
+// {
+//     console.log(i);
+// }
+
+
+for (i = 2; i<=10;i+=2)
+{
+    console.log(i);
+}
