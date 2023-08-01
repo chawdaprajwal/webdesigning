c8d5a6a78bb379a84d28ef46d8663d0d31be5b4e
chawdalprajwal <lopezloyla@gmail.com>
2023-08-01 11:26:43 +0530

    save file

--- MOD 2/JS/object_creat.js
+++ MOD 2/JS/object_creat.js
@@ -1,7 +1,32 @@
-student ()
-{
-    name :'Rajesh';
-    age : 18;
-    course : 'Cyber'
-}
-console.log(student.name)
+// //Creating an object using object literal notation
+// const student =
+// {
+//     fname :"Rajesh",
+//     lname :"Koothrapali",
+//     age : 27,
+//     occupation: "Astro physicist..",
+//method inside the object
+//     sayHello: function(){
+//         console.log(`Hello , my name is ${this.fname} ${this.lname}.I am ${this.age} years old.`);
+//     }
+// };
+// //accessing object properties using dot notation
+// console.log(student.fname);
+// console.log(student.lname);
+// console.log(student.age);
+// console.log(student.occupation);
+
+// //Accessing the function
+// student.sayHello();
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ MOD 2/function_obj.js
@@ -0,0 +1,9 @@
+//Creating an obj using constructor function
+function Car(make,model, year){
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    console.log( this.make +"" + this.model +""+ this.Car)
+}
+
+const Car1=
